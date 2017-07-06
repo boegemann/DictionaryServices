03e5523efe3e52b480f689b531d9251cd0bcf3a6
ibogemann <ingo@whiteclarkegroup.com>
2017-07-06 08:26:05 +0100

    First service

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Ingo' });
+router.get('/APP', function (req, res, next) {
+  res.json({
+    App: {
+      id: "Demo",
+      title: "Demo App",
+      homeScreen: home,
+      header: {
+        title: "WCG Dictionary Manager"
+      },
+      Screens: {
+        home: {
+          route: home
+        }
+      }
+    }
+  });
 });
 
 module.exports = router;
