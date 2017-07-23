6205640b1fa83c96f0f1371fbea31625850d18c5
ibogemann <ingo@whiteclarkegroup.com>
2017-07-23 11:14:07 +0100

    Just a value change for grid testing

--- services/dictionary/grid.js
+++ services/dictionary/grid.js
@@ -1,5 +1,32 @@
 var exports = module.exports = {};
 
 exports.getInitialColumns = function (next) {
-  next ({a:"b"});
-}
+  next([
+    {
+      name: 'Name',
+      dataIndex: 'name',
+      width: '50%'
+    }, {
+      name: 'Age',
+      dataIndex: 'age',
+      width: '50%'
+    }
+  ]);
+};
+
+exports.getInitialData = function (next) {
+  next([
+    {
+      name: 'Michael Jordan',
+      age: '20'
+    },
+    {
+      name: 'Charles Barkley',
+      age: '47'
+    },
+    {
+      name: 'Ingo',
+      age: '25'
+    }
+  ]);
+};
