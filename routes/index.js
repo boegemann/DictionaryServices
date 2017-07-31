f23b9af55485ef87d12f312cac2c844671a70183
ibogemann <ingo@whiteclarkegroup.com>
2017-07-31 10:35:01 +0100

    Moved to dynamic services descriptors throughout

--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,35 @@
 var express = require('express');
 var router = express.Router();
 var security = require("../security/jwt");
-var userService = require("../dao/users");
-var showDictionaryEntry = require("../services/dictionary/grid").showDictionaryEntry;
-var dictionaryFilter = require("../services/dictionary/grid").dictionaryFilter
 
 
 var createNavigationActions = require("../uimanagement/ApplicationState").createNavigationActions;
 
 
 
+// const serviceMap = {
+//   navigation: createNavigationActions,
+//   login: login,
+//   showDictionaryEntry:showDictionaryEntry,
+//   dictionaryFilter:dictionaryFilter
+// };
 
-const login = function (params, token, currentPath, next) {
-  userService.findLogin(params.username, params.password, function (err, user) {
-    if (!user) {
-      var nextNavigation = {
-        oldPath: params.nextPath,
-        newPath: currentPath,
-        data: {errorMessage:"The username or password don't match"}
-      }
-      createNavigationActions(nextNavigation, null, currentPath, next);
-    }else{
-      var token = security.createAccessToken(user);
-      var nextNavigation = {
-        oldPath: currentPath,
-        newPath: params.nextPath,
-        data: null
-      }
-      createNavigationActions(nextNavigation, token, currentPath, next);
-    }
-  });
-};
 
-const serviceMap = {
-  navigation: createNavigationActions,
-  login: login,
-  showDictionaryEntry:showDictionaryEntry,
-  dictionaryFilter:dictionaryFilter
-};
+const getService = function (serviceDescriptor){
+  if (serviceDescriptor==="navigation") return createNavigationActions // UI Service and not dynamically required
+  try{
+    var serviceStringArray = serviceDescriptor.split(":");
+    var service = require('../services/' + serviceStringArray[0])[serviceStringArray[1]];
+    return service;
+  }catch(e){
+    return null;
+  }
+}
 
 
 /* GET home page. */
 router.post('/ACTIONS', function (req, res) {
-  var service = serviceMap[req.body.service];
+  var service = getService(req.body.service);
   var token = security.verifyAndTouch(req.body.token);
   if (service != null) {
     service(req.body.params, token, req.body.currentPath, function (result) {
@@ -50,6 +37,7 @@ router.post('/ACTIONS', function (req, res) {
     })
   } else {
     console.error("Service with id: " + req.body.service + " was not found!");
+    res.status(500).send("Service with id: " + req.body.service + " was not found!")
   }
 
 });
---
+++ services/users/login.js
@@ -0,0 +1,27 @@
+var createNavigationActions = require("../../uimanagement/ApplicationState").createNavigationActions;
+var userService = require("../../dao/users");
+var security = require("../../security/jwt");
+
+var exports = module.exports = {};
+
+
+exports.login = function (params, token, currentPath, next) {
+  userService.findLogin(params.username, params.password, function (err, user) {
+    if (!user) {
+      var nextNavigation = {
+        oldPath: params.nextPath,
+        newPath: currentPath,
+        data: {errorMessage:"The username or password don't match"}
+      }
+      createNavigationActions(nextNavigation, null, currentPath, next);
+    }else{
+      var token = security.createAccessToken(user);
+      var nextNavigation = {
+        oldPath: currentPath,
+        newPath: params.nextPath,
+        data: null
+      }
+      createNavigationActions(nextNavigation, token, currentPath, next);
+    }
+  });
+};
